d0c64cbb2e378dbbe945cc70b4b30fc32d91ce48
Abdullah-2238 <155958204+Abdullah-2238@users.noreply.github.com>
2024-02-24 00:29:49 +0500

    Add files via upload

---
+++ Assignment 9A/script.js
@@ -0,0 +1,11 @@
+const civic = {
+    compay : "Honda",
+    model : "2023",
+    color : "white",
+    drive : function(){
+        return console.log("The car is being driven");
+    }};
+   
+
+console.log(civic);
+civic.drive();
---
+++ Assignment 9B/script.js
@@ -0,0 +1,14 @@
+const Oracle = {
+    companyName : "Oracle",
+    Founder : "Larry Ellison",
+    yearFounded : "June 16, 1977",
+    services : ["Java","management system","database software"]
+}
+console.log(Oracle.companyName);
+console.log(Oracle.Founder);
+console.log(Oracle.yearFounded);
+console.log("Services");
+console.log("--------");
+console.log(Oracle.services[0]);
+console.log(Oracle.services[1]);
+console.log(Oracle.services[2]);
